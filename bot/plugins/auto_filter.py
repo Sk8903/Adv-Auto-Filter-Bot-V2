cc0ab6bbc8af080766d6a1e2ab71e707e1d76a57
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2021-08-23 08:07:53 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"கிடைத்த{(len_results)} எபிசோட் 👇👇: <code>{query}</code>",
+                text=f"கிடைத்த{(len_results)} எபிசோட் 👇👇 🔷 Powered By - @Sk_Tv_Official: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
